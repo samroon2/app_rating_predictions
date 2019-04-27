280675ab6af5cde1d9ba6b1cc5feee0aedda9720
samroon2 <sam.roon2454@gmail.com>
2019-04-26 21:41:25 -0400

    Notebook showing outputs, api tidy up.

--- scoring_service/scoring_service.py
+++ scoring_service/scoring_service.py
@@ -1,23 +1,27 @@
 import keras
 import os, json, sys, pickle
+import tensorflow as tf
 from flask import Flask, jsonify, request
 from keras.preprocessing.sequence import pad_sequences
 from sklearn.externals import joblib
 sys.path.insert(0, '../code/')
 from text_cleaning import TextClean
-import tensorflow as tf
 
+
+# https://github.com/tensorflow/tensorflow/issues/24496
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 session = tf.Session(config=config)
+
+# Open project info and load tokenizer.
 with open('project_info.json') as f:
 	info = json.loads(f.read())
-
 with open("./models/"+info['tokenizer'], 'rb') as handle:
     tokenizer = pickle.load(handle)
 
 cleaner = TextClean('.')
 
+# Load model, store in memory to reduce request fulfillment time (loading the model every request takes time and can cause problems).
 model = keras.models.load_model("./models/"+info['model'])
 model._make_predict_function()
 
@@ -25,31 +29,32 @@ app = Flask(__name__)
 
 @app.route('/score', methods=['POST'])
 def predict_stars():
+	'''API scoring function, requires http POST request to contain 'title' and 'review'.
+	'''
 
 	try:
 		postjson = request.json
 	except Exception as e:
 		raise e
 
+	# Check request isn't empty or missing required information.	
+	if not postjson or [x for x in ['review', 'title'] if x not in postjson.keys()]:
+		return(bad_request())
+
 	title = postjson['title']
 	review = postjson['review']
 
+	# Process request data.
 	clean_text = cleaner.clean(title+'. '+review)
 
-	print(clean_text)
-
 	X = tokenizer.texts_to_sequences([clean_text])
-	print(X)
 	X = pad_sequences(X, info['max_sequence'])
 
-	print(X.shape)
-
-	if not postjson:
-		return(bad_request())
-
+	# Make prediction.
 	pred = model.predict(X)
 
-	apiresponse = jsonify({'stars':pred.argmax(axis=1).tolist()[0]})
+	# Return prediction.
+	apiresponse = jsonify({'stars':pred.argmax(axis=1).tolist()[0]+1})
 	apiresponse.status_code = 200
 
 	return (apiresponse)
